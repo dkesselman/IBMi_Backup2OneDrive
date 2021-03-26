05d2cd9457f4acadf2273e01934fc244a47f26db
Diego E. KESSELMAN <42894928+dkesselman@users.noreply.github.com>
2021-03-25 22:36:10 -0600

    Add files via upload

--- mnuod_const.sh
+++ mnuod_const.sh
@@ -1,5 +1,6 @@
         E='echo -e';e='echo -en';trap "R;exit" 2                                
       ODCMD='onedrivecmd'                                   
-  IFSPATH='/od2cloud'      
+  IFSPATH='/backup2od'      
    LIBLST=$IFSPATH'/liblist.lst'                                          
       ESC=$( $e "\e")
+    num_procs=8
